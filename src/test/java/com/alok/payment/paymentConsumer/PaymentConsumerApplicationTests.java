5cd03ffe43d3b2621a924a1de604ce50501c6295
FlutterOTA Setup <flutterota@example.com>
2025-11-14 09:24:41 +0000

    Initialize paymentConsumer project with Spring Boot and Maven setup

---
+++ src/test/java/com/alok/payment/paymentConsumer/PaymentConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok.payment.paymentConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
